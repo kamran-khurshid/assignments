776866c849c6de409860e1364f95352c70bfcaaf
kamran-khurshid <lonekamran2700@gmail.com>
2024-10-18 00:34:01 -0700

    Add files via upload

---
+++ assignments/max_in3.py
@@ -0,0 +1,5 @@
+number1 = int(input("enter number1:"))
+number2 = int(input("enter number2:"))
+number3 = int(input("enter number3:"))
+if number1 > number2 > number3 :
+    print(f"{number1} is greater than {number2} and {number3}")
---
+++ assignments/write a python program to check  wheater a charater is alphabet 7.py
@@ -0,0 +1,6 @@
+alphabets='qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
+char=input("enter any character")
+if char in alphabets:
+    print("it is an alphabets")
+else:
+    print("it is not an alphabets")
